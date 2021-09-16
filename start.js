12c806c0114aa44ff80444bc7d2b93efd708439a
Shireesh <shireesh@C02CH00QPWD7.local>
2021-09-16 14:32:24 +0530

    correcting the url

--- start.js
+++ start.js
@@ -2,7 +2,10 @@ const app = require('./app')
 const mongoose = require('mongoose');
 
 mongoose
-	.connect(process.env.DB_URL)
+	.connect(process.env.DB_URL, {
+        useNewUrlParser: false,
+        useUnifiedTopology: true
+    })
 	.then(() => {
         app.listen(process.env.PORT, () => {
             console.log("Server has started!")
